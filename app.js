3790cc5dc38bd04deb04baa1e1034a210e7cceb0
Aminroopkhs <poornimakhs06@gmail.com>
2025-07-24 18:46:38 +0530

    Error handling is implemented completely

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ let port=8080;
 const path= require("path");
 const Listing= require("./models/listing.js");
 const methodOverride= require("method-override");
-app.use(methodOverride("_method"));
 const ejsMate = require("ejs-mate");
+const wrapAsync= require("./utils/wrapasync.js");
+const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema}= require("./schema.js");
 // ejs 
 app.engine('ejs',ejsMate);
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"/public")));
+app.use(methodOverride("_method"));
 // mongodb
 const mongoose= require('mongoose');
 main().then(()=>{
@@ -21,6 +24,17 @@ main().then(()=>{
 });
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+// converting validation (JOI) into a middlware
+const validateListing= (req,res,next)=>{
+    let {error}= listingSchema.validate(req.body);
+    if (error){
+        let errMsg= error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else{
+        next();
+    }
 }
 
 // get request 
@@ -34,55 +48,76 @@ app.listen(port,()=>{
 
 // lists all the villas and other things on the page
 // index route
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     const all_listings= await Listing.find({});
         res.render("./listings/index.ejs",{all_listings});
-    })
+    }))
 // new route--> get request /listings/new --> returns a form to create new listing 
 // create route---> post request /listings/new or /listings---> saves the newly added listing to the databse
 app.get("/listings/new",(req,res)=>{
     res.render("./listings/new.ejs")
 })
-app.post("/listings",async(req,res)=>{
+app.post("/listings",validateListing, wrapAsync(async(req,res,next)=>{
     // let {title,description,image,price,country,location}= req.body; one of the ways to print
     // let listing= req.body.listing;
+    // let result= listingSchema.validate(req.body);
+    // if (result.error){
+    //     throw new ExpressError(400, result.error);
+    // }
     const new_listing= new Listing(req.body.listing);
-    // console.log(listing);
+        // checking only the entire listing and not the individual fields.
+        // if (!req.body.listing){
+        //     throw new ExpressError(400,"Send Valid data for listing");
+        // }
     await new_listing.save();
     res.redirect("/listings");
-})
+}))
 
 // edit and update route
 // get request /listings/:id/edit --> edit form rendering --> submit
 //put request /listings/:id
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id}= req.params;
-    id= id.replace(":","");
+    // id= id.replace(":","");
     const listing= await Listing.findById(id);
     res.render("./listings/edit.ejs",{listing});
-})
+}))
 
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
+    // if (!req.bosy.listing){
+    //         throw new ExpressError(400,"Send Valid data for listing");
+    //     };
     let {id}= req.params;
-    id= id.replace(":","");
+    // id= id.replace(":","");
     const listing= await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect("/listings");
-})
+}))
 
 // delete request /listings/:id
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
     let {id}=req.params;
-    id=id.replace(":","");
+    // id=id.replace(":","");
     const listing= await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-})
+}))
+
+
 // show route when clicked it specifically shows a particular villa (read operation)
-app.get("/listings/:id",async (req,res)=>{
+app.get("/listings/:id",wrapAsync(async (req,res)=>{
     let {id}= req.params;
-    id= id.replace(":","");
+    // id= id.replace(":","");
     const listing= await Listing.findById(id);
     res.render("./listings/show.ejs",{listing});
+}))
+
+// // error for any other route except the defined ones
+// app.all("*",(req,res,next)=>{
+//     next(new ExpressError (404, "Page not found!"));
+// })
+
+//custom express error
+app.use((err,req,res,next)=>{
+    let {status=500, message="Something Went Wrong!"}= err;
+    // res.status(status).send(message);
+    res.status(status).render("error.ejs",{err})
 })
-
-
-
