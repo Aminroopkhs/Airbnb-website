75bf15860828251cb480a50d3f9816f77725daaa
Aminroopkhs <poornimakhs06@gmail.com>
2025-08-06 17:36:47 +0530

    added login and signup fucntionalities with smooth redirection after authentication

--- middleware.js
+++ middleware.js
@@ -1,7 +1,17 @@
 module.exports.isLoggedIn= (req,res,next)=>{
+    // console.log(req.path,"..",req.originalUrl)
     if(!req.isAuthenticated()){
+        // infor of the user redirect url
+        req.session.redirectUrl= req.originalUrl;
+        // console.log(req.session.redirectUrl);
         req.flash("error","you must be loggedIn to create Listing")
         return res.redirect("/login")
     }
     next();
     }
+module.exports.saveredirectUrl=(req,res,next)=>{
+    if (req.session.redirectUrl){
+        res.locals.redirectUrl= req.session.redirectUrl
+    }
+    next();
+}
--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,15 @@ const router= express.Router({mergeParams:true});
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapasync.js");
 const passport=require("passport");
+const {saveredirectUrl}= require("../middleware.js")
 router.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 })
-router.post("/login",passport.authenticate('local', { failureRedirect: '/login', failureFlash:true }),async(req,res)=>{
+router.post("/login",saveredirectUrl,passport.authenticate('local', { failureRedirect: '/login', failureFlash:true }),
+    async(req,res)=>{
     req.flash("success","Welcome back to Wanderlust you are logged in");
-    res.redirect("/listings");
+    let redirectUrl=res.locals.redirectUrl || "/listings"
+    res.redirect(redirectUrl);
 })
 router.get("/signup",async(req,res)=>{
     res.render("users/signup.ejs");
@@ -25,9 +28,14 @@ router.post("/signup",wrapAsync(async(req,res)=>{
     // console.log(req.params);
         const newUser= new User({email,username});
         const reg_user=await User.register(newUser,password);
-        console.log(reg_user);
+        // console.log(reg_user);
+        req.login(reg_user,(err)=>{
+            if(err){
+            return next(err);
+        }
         req.flash("success","You are registered! Enjoy the Website!");
         res.redirect("/listings");
+        })
     }
     catch(e){
         req.flash("error", e.message);
@@ -42,4 +50,13 @@ router.post("/signup",wrapAsync(async(req,res)=>{
     // res.redirect("/listings");
 
 }));
+router.get("/logout",(req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+            next(err);
+        }
+        req.flash("success","You are logged out now");
+        res.redirect("/listings");
+    })
+})
 module.exports= router;
